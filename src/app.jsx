5de723224a32544d715a5576d0097521561d65a4
suhyeon21 <suhyeon.kan@gmail.com>
2022-02-03 17:19:58 +0900

    feat: 증가, 감소, 삭제 기능 구현

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import "./app.css";
-import Habit from "./components/habit.jsx";
+import Habits from "./components/habits.jsx";
+import Navbar from "./components/navbar";
 
-function App() {
-  return <Habit />;
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits]; //Spread Operator, 직접적으로 state 수정하면 좋지 않기 때문
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habit }); //key와 value의 이름이 동일한 경우 하나 생략 가능
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habit });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar
+          totalCount={this.state.habits.filter((item) => item.count > 0).length}
+        />
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
