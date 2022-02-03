66c06455fb9e7bcd4163eb391594f4ed7e233601
suhyeon21 <suhyeon.kan@gmail.com>
2022-02-03 17:14:15 +0900

    feat: Create habit.jsx component

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,25 +1,24 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
-  state = {
-    count: 0,
-  };
-
+  //외부로부터 받은 데이터를 보여주기만 하는 컴포넌트이므로 state 필요 없음
   handleIncrement = () => {
-    //state 오브젝트 안에 있는 count를 증가 한뒤 state를 업데이트
-
-    this.setState({ count: this.state.count + 1 });
+    this.props.onIncrement(this.props.habit.name);
   };
 
   handleDecrement = () => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count - 1 });
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
   };
   render() {
+    const { name, count } = this.props.habit;
     return (
       <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
         <button
           className="habit-button habit-increase"
           onClick={this.handleIncrement}
@@ -32,7 +31,10 @@ class Habit extends Component {
         >
           <i className="fas fa-minus-square"></i>
         </button>
-        <button className="habit-button habit-delete">
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
           <i className="fas fa-trash"></i>
         </button>
       </li>
